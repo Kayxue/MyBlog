d17a548f6db5ebeaf0d81b2175cdd6b0dcd5702b
Kayxue <s0990006@gmail.com>
2025-02-25 01:06:15 +0800

    Change avatar and name

--- src/config.ts
+++ src/config.ts
@@ -52,7 +52,7 @@ export const navBarConfig: NavBarConfig = {
 
 export const profileConfig: ProfileConfig = {
   avatar: 'assets/images/avatar.jpg',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-  name: 'Lorem Ipsum',
+  name: 'KayXue',
   bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
   links: [
     {
